87b0a609065b23b74e04608aa42f634e9d202b53
lemonkazi <lemonpstu09@gmail.com>
2017-03-20 11:58:04 +0600

    ad edit functionality bug solved

--- findCity.php
+++ findCity.php
@@ -14,7 +14,7 @@ if (!$con) {
 }
 mysql_select_db($db);
 //$query="SELECT id,city FROM city WHERE countryid='$countryId' AND stateid='$stateId'";
-$query = "SELECT t1.region_id as id, t2.value as regionname FROM " . $table_prefix . "country_region as t1 INNER JOIN " . $table_prefix . "eav_attribute_option_value as t2 where t1.country_id = " . $countryId . " and  t1.state_id = " . $stateId . " and t2.store_id = 0 and t1.region_id = t2.option_id ORDER BY regionname ASC";
+$query = "SELECT t1.region_id as id, t2.value as regionname FROM " . $table_prefix . "country_region as t1 INNER JOIN " . $table_prefix . "eav_attribute_option_value as t2 where t1.country_id = " . $countryId . " and  t1.state_id = " . $stateId . " and t2.store_id = 0 and t1.region_id = t2.option_id group by id ORDER BY regionname ASC";
 $result = mysql_query($query);
 ?>
 <select name="region_city" id="region_city" class="cate-se validate-select">
